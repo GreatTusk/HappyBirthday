73dd6b0a8ada64e6fb3f6cd7c569cd2db55adcb3
f776 <fern.belmar@duocuc.cl>
2024-10-31 19:20:55 -0300

    finished tutorial

--- composeApp/src/commonMain/kotlin/com/f776/happybirthday/App.kt
+++ composeApp/src/commonMain/kotlin/com/f776/happybirthday/App.kt
@@ -1,29 +1,29 @@
 package com.f776.happybirthday
 
-import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import happybirthday.composeapp.generated.resources.Res
+import happybirthday.composeapp.generated.resources.androidparty
+import happybirthday.composeapp.generated.resources.happy_birthday_text
+import happybirthday.composeapp.generated.resources.signature_text
 import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.resources.stringResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
-import happybirthday.composeapp.generated.resources.Res
-import happybirthday.composeapp.generated.resources.compose_multiplatform
 
 @Composable
 @Preview
@@ -33,8 +33,10 @@ fun App() {
             modifier = Modifier.fillMaxSize(),
             color = MaterialTheme.colors.background
         ) {
-            GreetingText("Happy Birthday Sam!", "From Fernando",
-                modifier = Modifier.padding(8.dp))
+            GreetingImage(
+                message = stringResource(Res.string.happy_birthday_text),
+                from = stringResource(Res.string.signature_text),
+            )
         }
     }
 }
@@ -49,13 +51,36 @@ fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
             text = message,
             fontSize = 90.sp,
             lineHeight = 116.sp,
-            textAlign = TextAlign.Center
+            textAlign = TextAlign.Center,
+            color = MaterialTheme.colors.primary
         )
         Text(
             text = from,
             fontSize = 36.sp,
             modifier = Modifier.padding(16.dp)
-                .align(alignment = Alignment.End)
+                .align(alignment = Alignment.CenterHorizontally),
+            color = MaterialTheme.colors.secondaryVariant
+        )
+    }
+}
+
+@Composable
+fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) {
+    val image = painterResource(Res.drawable.androidparty)
+    Box(modifier = modifier) {
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            alpha = 0.5f
+        )
+
+        GreetingText(
+            message = message,
+            from = from,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp)
         )
     }
 }
--- composeApp/src/iosMain/kotlin/com/f776/happybirthday/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/f776/happybirthday/MainViewController.kt
@@ -1,5 +1,3 @@
 package com.f776.happybirthday
 
-import androidx.compose.ui.window.ComposeUIViewController
-
-fun MainViewController() = ComposeUIViewController { App() }
+//fun MainViewController() = ComposeUIViewController { App() }
